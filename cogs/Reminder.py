4354d33a43f5c5b588ae6b2d5e4b5a97cb27b297
Matthew <matthew.brough@hotmail.com>
2022-05-02 22:15:04 +0100

    initial table and insert

--- cogs/Reminder.py
+++ cogs/Reminder.py
@@ -2,7 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands, tasks
 from typing import Optional
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 
 
 class Reminder(commands.Cog, app_commands.Group):
@@ -13,13 +13,39 @@ class Reminder(commands.Cog, app_commands.Group):
 
     @commands.Cog.listener()
     async def on_ready(self):
+        await self.execute(
+            "CREATE TABLE IF NOT EXISTS reminder(guild BIGINT NOT NULL, member BIGINT NOT NULL, creationtime TIMESTAMP WITH TIME ZONE NOT NULL, expiretime INTERVAL SECOND NOT NULL, jumplink TEXT NOT NULL)",
+            [])
         print("Reminder cog online")
 
-    @app_commands.command(name = "create")
-    async def ReminderCreate(self, year: int = dt.now().year, month: int = dt.now().month, day: int = dt.now().day,
+    async def fetch(self, sql: str, args: list):
+        async with self.bot.pool.acquire() as conn:
+            async with conn.transaction():
+                return await conn.fetch(sql, *args)
+
+    async def fetchval(self, sql: str, args: list):
+        async with self.bot.pool.acquire() as conn:
+            async with conn.transaction():
+                return await conn.fetchval(sql, *args)
+
+    async def execute(self, sql: str, args: list):
+        async with self.bot.pool.acquire() as conn:
+            async with conn.transaction():
+                return await conn.fetchval(sql, *args)
+
+    @app_commands.command(name = "create",
+                          description = "Set a DM reminder for all your important things (all fields are optional)")
+    @app_commands.describe(year = "YYYY", month = "MM", day = "DD", hour = "HH", minute = "MM", second = "SS",
+                           message = "reminder message")
+    async def ReminderCreate(self, interaction: discord.Interaction,
+                             year: int = dt.now().year, month: int = dt.now().month, day: int = dt.now().day,
                              hour: int = dt.now().hour, minute: int = dt.now().minute, second: int = dt.now().second,
                              message: str = "..."):  # TODO:args
-        pass
+        expiretime = timedelta(year, month, day, hour, minute, second)
+        await self.execute(
+            "INSERT INTO reminder(guild, member, creationtime, expiretime, jumplink) VALUES($1, $2, $3, $4, $5)",
+            [interaction.guild.id, interaction.user.id, interaction.created_at, expiretime.total_seconds,
+             interaction.message.jump_url])  # TODO:jump link errors, find new way to link back
 
     """
     reminder create YY, MM, DD, HH, MM, SS, message
