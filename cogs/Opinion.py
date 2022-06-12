e8102efc88bb40ee77ef1b75aa55ff7922464f94
Matthew <matthew.brough@hotmail.com>
2022-06-12 19:47:08 +0100

    blacklist prefix and removal of links

--- cogs/Opinion.py
+++ cogs/Opinion.py
@@ -1,34 +1,38 @@
-from datetime import datetime
 import discord
 from discord import app_commands
 from discord.ext import commands, tasks
-from typing import Optional
+from re import sub
 
 class Opinion(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.DeleteOld.start()
-        self.prefixes = [":", ".", "!", "?", "/", ">", "+", '"']
 
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.bot.execute("CREATE TABLE IF NOT EXISTS Opinion(guild BIGINT NOT NULL, timestamp TIMESTAMP WITH TIME ZONE NOT NULL, content TEXT DEFAULT NULL, attachment TEXT DEFAULT NULL)")
+        await self.bot.execute("CREATE TABLE IF NOT EXISTS Opinion(guild BIGINT NOT NULL, timestamp TIMESTAMP WITH TIME ZONE NOT NULL, content TEXT DEFAULT NULL)")
+        await self.bot.execute("CREATE TABLE IF NOT EXISTS PrefixBlacklist(guild BIGINT NOT NULL, prefix TEXT NOT NULL)")
         print("Opinion cog online")
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author.bot is False and message.channel.is_nsfw() is False:
-            if not any(message.content.startswith(prefix) for prefix in self.prefixes):
-                if message.attachments:
-                    message.attachments = str(message.attachments[-1])
-                else:
-                    message.attachments = ""
-                await self.bot.execute("INSERT INTO Opinion(guild, timestamp, content, attachment) VALUES ($1,$2,$3,$4)", message.guild.id, message.created_at, message.clean_content, message.attachments)
+        if any((message.author.bot, message.channel.is_nsfw(), message.attachments)):
+            return
+        prefixes = await self.bot.fetch("SELECT prefix FROM PrefixBlacklist WHERE guild = $1", message.guild.id)
+        if any(message.content.startswith(prefix["prefix"]) for prefix in prefixes):
+            return
+        content = sub(r"http\S+", "", message.clean_content)
+        await self.bot.execute("INSERT INTO opinion(guild, timestamp, content) VALUES ($1, $2, $3)", message.guild.id, message.created_at, content)
 
     @app_commands.command(name="opinion", description="Ask worst bot for its opinion on your super important questions")
     async def opinion(self, interaction: discord.Interaction, opinion: str = None):
         row = await self.bot.fetch("SELECT content, attachment FROM Opinion WHERE GUILD=$1 ORDER BY random() LIMIT 1", interaction.guild.id)
         await interaction.response.send_message(content=f"""{row[0]["content"]}\n{row[0]["attachment"]}""")
+
+    @app_commands.command(name = "prefix-blacklist", description = "blacklist prefixes used by other bots in opinion forming")
+    async def PrefixBlacklist(self, interaction: discord.Interaction, prefix: str):
+        await self.bot.execute("INSERT INTO prefixblacklist(guild, prefix) VALUES ($1, $2)", interaction.guild_id, prefix)
+        await interaction.response.send_message(f"messages starting with {prefix} will be ignored")
 
     @tasks.loop(hours=24, reconnect=True)
     async def DeleteOld(self):
