5cf5b8027ae77d8b4bd611034d6121caab94da21
artemOP <matthew.brough@hotmail.com>
2022-11-15 00:15:10 +0000

    refactor: removed commands built into discord ui

--- cogs/BaseCommands.py
+++ cogs/BaseCommands.py
@@ -38,27 +38,6 @@ class BaseCommands(commands.Cog):
         deleted = await interaction.channel.purge(limit = amount, check = self.is_me, bulk = True if amount > 1 else False, after = interaction.created_at - timedelta(weeks = 2), oldest_first = False)
         await interaction.followup.send(content = f"deleted {len(deleted)} messages")
 
-    @app_commands.command(name = "kick")
-    @app_commands.default_permissions(kick_members = True)
-    async def kick(self, interaction: Interaction, member: discord.Member, *, reason: str = None):
-        await member.kick(reason = reason)
-        await interaction.response.send_message(f"kicked {member.name} for: `{reason}`")
-
-    @app_commands.command(name = "ban")
-    @app_commands.default_permissions(ban_members = True)
-    async def ban(self, interaction: Interaction, member: discord.Member, *, reason: str = None):
-        await member.ban(reason = reason)
-        await interaction.response.send_message(f"banned {member.name} for: `{reason}`")
-
-    @app_commands.command(name = "unban")
-    @app_commands.default_permissions(ban_members = True)
-    async def unban(self, interaction: Interaction, member: str):
-        try:
-            await interaction.guild.unban(discord.Object(int(member)))
-            await interaction.response.send_message(f"Unban succeeded")
-        except discord.NotFound:
-            await interaction.response.send_message(f"User not found", ephemeral = True)
-
     @app_commands.command(name = "rtd", description = "role some dice")
     @app_commands.describe(dice="number of dice to roll", sides="number of faces on each die", ephemeral="Set to false to be visible by all")
     async def roll_the_dice(self, interaction: Interaction, dice: int = 1, sides: int = 6, ephemeral: bool = True):
