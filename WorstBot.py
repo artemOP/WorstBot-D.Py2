1490631713dc87fb0000e391d097bc6f12d4b10f
artemOP <matthew.brough@hotmail.com>
2022-11-26 12:09:00 +0000

    Refactor startup to allow for future expansion

--- WorstBot.py
+++ WorstBot.py
@@ -1,14 +1,17 @@
-import discord
-from discord.ext import commands
-from os import listdir, environ
-import asyncpg
+import asyncio
 import datetime
-from dotenv import load_dotenv
+from logging import WARN, INFO, DEBUG
+from os import environ, listdir
+
+import discord
+from discord.ext import commands as discord_commands
+
+import asyncpg
 from aiohttp import ClientSession
-from logging import DEBUG, WARN
+from dotenv import load_dotenv
 
 
-class WorstBot(commands.Bot):
+class WorstBot(discord_commands.Bot):
     def __init__(self, command_prefix, activity, intents):
         super().__init__(command_prefix, intents = intents)
         self.pool = None
@@ -16,67 +19,60 @@ class WorstBot(commands.Bot):
         self.activity = activity
 
     async def setup_hook(self) -> None:
-        bot.pool = await asyncpg.create_pool(database = environ.get("postgresdb"), user = environ.get("postgresuser"), password = environ.get("postgrespassword"), command_timeout = 10, max_size = 100, min_size = 25)
-        bot.session = ClientSession()
-        bot.post = self.post
-        bot.get = self.get
-        bot.getstatus = self.getstatus
-        bot.fetch = self.fetch
-        bot.fetchrow = self.fetchrow
-        bot.fetchval = self.fetchval
-        bot.execute = self.execute
-        bot.current = self.current
+        self.pool = await asyncpg.create_pool(database = environ.get("postgresdb"), user = environ.get("postgresuser"), password = environ.get("postgrespassword"), command_timeout = 10, max_size = 100, min_size = 25)
+        self.session = ClientSession()
+        self.post = self.post
+        self.get = self.get
+        self.getstatus = self.getstatus
+        self.fetch = self.fetch
+        self.fetchrow = self.fetchrow
+        self.fetchval = self.fetchval
+        self.execute = self.execute
+        self.current = self.current
 
         for filename in listdir("cogs"):
             if filename.endswith(".py") and not filename.startswith("-"):
-                await bot.load_extension(f'cogs.{filename[:-3]}')
+                await self.load_extension(f'cogs.{filename[:-3]}')
                 pass
 
     async def on_ready(self):
         print(f"Connected as {self.user} at {datetime.datetime.now().strftime('%d/%m/%y %H:%M')}")
 
-    @staticmethod
-    async def post(*, url: str, params: dict = None, headers: dict = None):
-        async with bot.session.post(url = url, params = params, headers = headers) as response:
+    async def post(self, *, url: str, params: dict = None, headers: dict = None):
+        async with self.session.post(url = url, params = params, headers = headers) as response:
             content = await response.json()
             content["status"] = response.status
             return content
 
-    @staticmethod
-    async def get(*, url: str, params: dict = None, headers: dict = None):
-        async with bot.session.get(url = url, params = params, headers = headers) as response:
+    async def get(self, *, url: str, params: dict = None, headers: dict = None):
+        async with self.session.get(url = url, params = params, headers = headers) as response:
             content = await response.json()
             if not isinstance(content, dict):
                 content = {"data": content[0] if len(content) == 1 else content}
             content["status"] = response.status
             return content
 
-    @staticmethod
-    async def getstatus(*, url: str, params: dict = None, headers: dict = None):
-        async with bot.session.get(url = url, params = params, headers = headers) as response:
+    async def getstatus(self, *, url: str, params: dict = None, headers: dict = None):
+        async with self.session.get(url = url, params = params, headers = headers) as response:
             return response.status
 
-    @staticmethod
-    async def fetch(sql: str, *args):
-        async with bot.pool.acquire() as conn:
+    async def fetch(self, sql: str, *args):
+        async with self.pool.acquire() as conn:
             async with conn.transaction():
                 return await conn.fetch(sql, *args)
 
-    @staticmethod
-    async def fetchrow(sql: str, *args):
-        async with bot.pool.acquire() as conn:
+    async def fetchrow(self, sql: str, *args):
+        async with self.pool.acquire() as conn:
             async with conn.transaction():
                 return await conn.fetchrow(sql, *args)
 
-    @staticmethod
-    async def fetchval(sql: str, *args):
-        async with bot.pool.acquire() as conn:
+    async def fetchval(self, sql: str, *args):
+        async with self.pool.acquire() as conn:
             async with conn.transaction():
                 return await conn.fetchval(sql, *args)
 
-    @staticmethod
-    async def execute(sql: str, *args):
-        async with bot.pool.acquire() as conn:
+    async def execute(self, sql: str, *args):
+        async with self.pool.acquire() as conn:
             async with conn.transaction():
                 return await conn.fetchval(sql, *args)
 
@@ -85,8 +81,14 @@ class WorstBot(commands.Bot):
         return "%" if not current else current
 
 
+async def start():
+    await asyncio.gather(discord_bot.start(environ.get("discord")), return_exceptions = True)
+
+
 load_dotenv()
-bot = WorstBot(command_prefix = commands.when_mentioned,
-               activity = discord.Game(name = "With ones and zeros"),
-               intents = discord.Intents.all())
-bot.run(environ.get("discord"), log_level = WARN)
+discord.utils.setup_logging(level = WARN)
+discord_bot = WorstBot(command_prefix = discord_commands.when_mentioned,
+                       activity = discord.Streaming(name = "With ones and zeros", url = "http://definitelynotarickroll.lol/", game = "a little bit of trolling", platform = "YouTube"),
+                       intents = discord.Intents.all())
+
+asyncio.run(start())
