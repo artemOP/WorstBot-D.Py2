3b1e913a7afa5d8e9359537f41793b797755a10e
artemOP <matthew.brough@hotmail.com>
2022-12-13 22:08:22 +0000

    Move commands to global synchronisation (public soontm???)

--- cogs/Sync.py
+++ cogs/Sync.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 from discord.ext.commands import Context
+from typing import Literal
 
 class Sync(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -20,29 +21,38 @@ class Sync(commands.Cog):
     @commands.hybrid_command(name = "sync", description = "Sync command")
     @commands.is_owner()
     @app_commands.default_permissions()
-    async def sync(self, ctx: Context) -> None:
+    @app_commands.describe(option = "*|/ to add|remove global sync, +|- to add|remove local sync")
+    async def sync(self, ctx: Context, option: Literal["*", "/", "+", "-"]) -> discord.Message:
         await ctx.defer(ephemeral = True)
-        guilds = []
-        for guild in self.bot.guilds:
-            self.bot.tree.clear_commands(guild = guild)
-            self.bot.tree.copy_global_to(guild = guild)
-            await self.bot.tree.sync(guild = guild)
-            guilds.append(guild.name)
-        guilds = "\n".join(guilds)
-        await ctx.send(f"Successfully synced to:\n{guilds}", ephemeral = True)
+        match option:
+            case "*":
+                await self.bot.tree.sync(guild = None)
+                return await ctx.send("Commands synced globally")
+
+            case "/":
+                self.bot.tree.clear_commands(guild = None)
+                await self.bot.tree.sync(guild = None)
+                return await ctx.send("Global commands removed")
+
+            case "+":
+                self.bot.tree.clear_commands(guild = ctx.guild)
+                self.bot.tree.copy_global_to(guild = ctx.guild)
+                await self.bot.tree.sync(guild = ctx.guild)
+                return await ctx.send("Commands synced locally")
+
+            case "-":
+                self.bot.tree.clear_commands(guild = ctx.guild)
+                await self.bot.tree.sync(guild = ctx.guild)
+                return await ctx.send("Local commands removed")
 
     @sync.error
     async def sync_error(self, ctx: Context, error):
         if isinstance(error, commands.NotOwner):
             await ctx.send(str(error), ephemeral = True)
+        elif isinstance(error, commands.BadLiteralArgument):
+            await ctx.send("Incorrect option passed, please try again")
         else:
-            print(error)
+            raise error
 
 async def setup(bot):
     await bot.add_cog(Sync(bot))
-
-
-# alpha = discord.Object(id = 700833272380522496)
-# bot.tree.clear_commands(guild = alpha)
-# bot.tree.copy_global_to(guild = alpha)
-# await bot.tree.sync(guild = alpha)
